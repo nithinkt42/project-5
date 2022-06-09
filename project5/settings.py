b53849c1c221bc4403a19db81336e25e9615fd35
nithinkt42 <nithinkt42@gmail.com>
2022-06-09 19:36:04 +0530

    1

--- project5/settings.py
+++ project5/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-)dw@&7^46_%8&a*r80t=^-ol_1xe=*0q-!f5-acs6gm7uk@kbb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
